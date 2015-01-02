3100f6cc4bfacd57f8ba9e337010f76b6080084d
Aaron Bieber <aaron@aaronbieber.com>
2015-01-02 17:16:30 -0500

    First draft config stuff -- broken.

--- includes/AB/Chroma/Config.php
+++ includes/AB/Chroma/Config.php
@@ -1,10 +1,30 @@
 <?php
 /**
- * The base class for collections of models.
+ * Interface to configuration options.
  *
  * @copyright 2015 Aaron Bieber, All Rights Reserved
  */
 namespace AB\Chroma;
 
 class Config {
+  private static $instance = null;
+  public $options = [];
+
+  public function __construct() {
+    $this->load();
+  }
+
+  public function get_instance() {
+    if (static::$instance === null) {
+      static::$instance = new static();
+    }
+
+    return $this->instance;
+  }
+
+  private function load() {
+    $config_yaml = file_get_contents('config/config.yml');
+    $this->options = \yaml_parse($config_yaml);
+    var_dump($this->options);
+  }
 }
