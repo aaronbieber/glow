dcb40508e6989156de62023853922a94d615fbf0
Aaron Bieber <aaron@aaronbieber.com>
2016-05-30 16:03:14 -0400

    Reindent, add route for single scene light

--- dispatch.php
+++ dispatch.php
@@ -16,74 +16,79 @@ define('LIBRARY_PATH', '/var/www/glow/htdocs/includes');
 require_once 'vendor/autoload.php';
 require_once 'includes/autoloader.php';
 
-Flight::route('/', function() {
-  $c = new \AB\Chroma\Controllers\Home();
-  $c->get();
+Flight::route('/', function () {
+    $c = new \AB\Chroma\Controllers\Home();
+    $c->get();
 });
 
-Flight::route('GET /heartbeat/get', function() {
-  $c = new \AB\Lamplighter\Controllers\Heartbeat();
-  $c->get();
+Flight::route('GET /heartbeat/get', function () {
+    $c = new \AB\Lamplighter\Controllers\Heartbeat();
+    $c->get();
 });
 
 Flight::route('GET /heartbeat/set', function () {
-  $c = new \AB\Lamplighter\Controllers\Heartbeat();
-  $c->set();
+    $c = new \AB\Lamplighter\Controllers\Heartbeat();
+    $c->set();
 });
 
-Flight::route('GET /lights', function() {
-  $c = new \AB\Chroma\Controllers\Lights();
-  $c->get();
+Flight::route('GET /lights', function () {
+    $c = new \AB\Chroma\Controllers\Lights();
+    $c->get();
 });
 
-Flight::route('GET /light/@number', function($number) {
-  $c = new \AB\Chroma\Controllers\Light();
-  $c->get($number);
+Flight::route('GET /light/@number', function ($number) {
+    $c = new \AB\Chroma\Controllers\Light();
+    $c->get($number);
 });
 
-Flight::route('PUT|POST /light/@number', function($number) {
-  $c = new \AB\Chroma\Controllers\Light();
-  $c->post($number);
+Flight::route('PUT|POST /light/@number', function ($number) {
+    $c = new \AB\Chroma\Controllers\Light();
+    $c->post($number);
 });
 
-Flight::route('POST /scene/@scene/choose', function($scene) {
-  $c = new \AB\Chroma\Controllers\SceneActionHandler();
-  $c->post($scene);
+Flight::route('POST /scene/@scene/choose', function ($scene) {
+    $c = new \AB\Chroma\Controllers\SceneActionHandler();
+    $c->post($scene);
 });
 
-Flight::route('POST /scene/choose', function() {
-  $c = new \AB\Chroma\Controllers\SceneActionHandler();
-  $c->post($scene);
+Flight::route('POST /scene/choose', function () {
+    $c = new \AB\Chroma\Controllers\SceneActionHandler();
+    $c->post($scene);
 });
 
-Flight::route('POST /scenes/by_name/@scene', function($scene) {
-  $c = new \AB\Chroma\Controllers\Scenes();
-  $c->post($scene);
+Flight::route('POST /scenes/by_name/@scene', function ($scene) {
+    $c = new \AB\Chroma\Controllers\Scenes();
+    $c->post($scene);
 });
 
-Flight::route('POST /scene', function() {
-  $c = new \AB\Chroma\Controllers\Scene();
-  $c->post();
+Flight::route('POST /scene', function () {
+    $c = new \AB\Chroma\Controllers\Scene();
+    $c->post();
 });
 
-Flight::route('PUT|POST /scene/@scene', function($scene) {
-  $c = new \AB\Chroma\Controllers\Scene();
-  $c->put($scene);
+Flight::route('PUT|POST /scene/@scene', function ($scene) {
+    $c = new \AB\Chroma\Controllers\Scene();
+    $c->put($scene);
 });
 
-Flight::route('GET /scenes', function() {
-  $c = new \AB\Chroma\Controllers\Scenes();
-  $c->get();
+Flight::route('/scene/@scene_id/light/@light_id', function ($scene_id, $light_id) {
+    $c = new \AB\Chroma\Controllers\Scene();
+    $c->postLight($scene_id, $light_id);
 });
 
-Flight::route('PUT /scenes', function() {
-  $c = new \AB\Chroma\Controllers\Scenes();
-  $c->put();
+Flight::route('GET /scenes', function () {
+    $c = new \AB\Chroma\Controllers\Scenes();
+    $c->get();
 });
 
-Flight::route('GET /config', function() {
-  $c = new \AB\Chroma\Controllers\Config();
-  $c->get();
+Flight::route('PUT /scenes', function () {
+    $c = new \AB\Chroma\Controllers\Scenes();
+    $c->put();
+});
+
+Flight::route('GET /config', function () {
+    $c = new \AB\Chroma\Controllers\Config();
+    $c->get();
 });
 
 Flight::start();
