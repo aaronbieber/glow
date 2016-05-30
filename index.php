c59b9806f80b4c2ced145e888d55e372dbc43303
Aaron Bieber <aaron@aaronbieber.com>
2016-05-30 13:07:36 -0400

    Populate scene/light relations properly

--- index.php
+++ index.php
@@ -43,6 +43,7 @@ $scenes->load($lights);
   <script src="assets/js/jquery-2.1.3.min.js" type="text/javascript"></script>
   <script src="assets/js/underscore-min.js"></script>
   <script src="assets/js/backbone-min.js" type="text/javascript"></script>
+  <script src="assets/js/backbone-nested-models.js" type="text/javascript"></script>
   <script src="assets/js/mustache.min.js" type="text/javascript"></script>
   <script src="assets/js/glow.js" type="text/javascript"></script>
   <script src="assets/js/farbtastic.js" type="text/javascript"></script>
@@ -52,7 +53,8 @@ $scenes->load($lights);
   <script type="text/javascript">
    $(document).ready(function() {
      // Start everything
-     app.sceneCollection = new app.SceneCollection(<?= json_encode($scenes->asArray()); ?>);
+     app.sceneCollection = new app.SceneCollection();
+     app.sceneCollection.set(<?= json_encode($scenes->asArray()); ?>);
      app.lightCollection = new app.LightCollection(<?= json_encode($lights->asArray()); ?>);
      app.navigationLinkCollection = new app.NavigationLinkCollection([
        (new app.NavigationLink({ alias: 'scenes', name: 'Scenes', active: true })),
